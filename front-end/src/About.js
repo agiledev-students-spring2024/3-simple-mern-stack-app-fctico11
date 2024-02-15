49b4788e205ac4ea406461311bcd52555bdf93c7
fctico11 <fac322@nyu.edu>
2024-02-15 13:08:55 -0500

    added more to about me

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -12,8 +12,21 @@ const About = () => {
     <div className="about">
       <h1>About Us</h1>
       <img src="/me.png" alt="NFT profile picture" />
-      <p>Hello all! My name is Fran. I'm in my last semester at NYU, and very excited to be taking Agile with you all! </p>
-      <p></p>
+      <p>Hello all! My name is Fran. I'm in my last semester at NYU, 
+        and very excited to be taking Agile! A little bit about me, 
+        I'm Costa Rican and love playing soccer in my free time.
+        My favorite team is Real Madrid and my current favorite player is Rodyrgo. You can often find me at a local pub supporting them on Sundays. I also have a passion for working out and
+        enjoy all outdoor-type activities. Side note, Christopher Nolan 
+        movies over everything!
+         </p>
+      <p>A big reason I chose to study CS is my fascination with crypto and blockchain technology.
+        I believe this tech has the potential to revolutionize the way we think about financial systems and data integrity.
+        In fact, the image you see here is an NFT from the Tensorian collection minted on the Solana network.
+        I believe in the innovations of crypto to restore some power to the individual through
+        taking away control from centralized entities like banks and governments. I hope to somehow contribute
+        to the field when I graduate. If you ever want to talk crypto, you can find me on discord @fac322!
+        
+      </p>
     </div>
   );
 };
