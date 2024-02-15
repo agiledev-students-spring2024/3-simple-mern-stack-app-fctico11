583e3d4bc5d74624db1deb3508076ac1dfc220e6
fctico11 <fac322@nyu.edu>
2024-02-15 12:28:57 -0500

    added image to about page

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 //import './About.css';
+import me from '../asset/me.png'; // Path to NFT image
+
 
 /**
  * A React component that represents the About page of the app.
@@ -9,6 +11,7 @@ const About = () => {
   return (
     <div className="about">
       <h1>About Us</h1>
+      <img src={me} alt="NFT profile picture" />
       <p>Hello all! My name is Fran</p>
       <p></p>
     </div>
